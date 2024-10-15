6dcb5390a4c0ea8211ebf55acf9c3c6e32bbfa01
Dexedus <karlfleming64@gmail.com>
2024-10-15 12:29:30 +0100

    added sub info and icons

--- index.js
+++ index.js
@@ -25,13 +25,16 @@ app.post("/submit", async (req, res) =>{
         const place = await axios.get("http://api.openweathermap.org/geo/1.0/direct?q="+req.body["location"]+"&limit=5&appid="+apiKey)
         const lat = JSON.stringify(place.data[0].lat)
         const lon = JSON.stringify(place.data[0].lon)
-        const result = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid="+apiKey);
+        const result = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid="+apiKey)
+
 
         res.render("index.ejs", {
-            temp: JSON.stringify(result.data.main.temp),
-            location: JSON.stringify(result.data.name),
-            light: JSON.stringify(result.data.visibility),
-            weather: JSON.stringify(result.data.weather[0].main),
+            temp: result.data.main.temp,
+            location: result.data.name,
+            light: result.data.visibility,
+            weather: result.data.weather[0].main,
+            wind: result.data.wind.speed,
+            humidity: result.data.main.humidity,
         });
     } catch (error){
         console.log(error);
