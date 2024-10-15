6c9a14a21046b5ceba1e51152c4d4081b8acbab6
Dexedus <karlfleming64@gmail.com>
2024-10-15 10:54:44 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import 'dotenv/config' 
 
 const app = express();
 const port = 3000;
+const apiKey = process.env.Key;
+
 
 app.use(express.static("public"));
 
@@ -19,10 +22,10 @@ app.post("/submit", async (req, res) =>{
     console.log(req.body["location"]);
 
     try{
-        const place = await axios.get("http://api.openweathermap.org/geo/1.0/direct?q="+req.body["location"]+"&limit=5&appid="+Key)
+        const place = await axios.get("http://api.openweathermap.org/geo/1.0/direct?q="+req.body["location"]+"&limit=5&appid="+apiKey)
         const lat = JSON.stringify(place.data[0].lat)
         const lon = JSON.stringify(place.data[0].lon)
-        const result = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid="+Key);
+        const result = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid="+apiKey);
 
         res.render("index.ejs", {
             temp: JSON.stringify(result.data.main.temp),
