fe0a551dc97b872a9c74fe66737db34cf68d2165
Dexedus <karlfleming64@gmail.com>
2024-10-15 10:13:10 +0100

    update

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs")
+})
+
+
+app.post("/submit", async (req, res) =>{
+
+    console.log(req.body["location"]);
+
+    try{
+        const place = await axios.get("http://api.openweathermap.org/geo/1.0/direct?q="+req.body["location"]+"&limit=5&appid="+Key)
+        const lat = JSON.stringify(place.data[0].lat)
+        const lon = JSON.stringify(place.data[0].lon)
+        const result = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid="+Key);
+
+        res.render("index.ejs", {
+            temp: JSON.stringify(result.data.main.temp),
+            location: JSON.stringify(result.data.name),
+            light: JSON.stringify(result.data.visibility),
+            weather: JSON.stringify(result.data.weather[0].main),
+        });
+    } catch (error){
+        console.log(error);
+        res.status(500);
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
